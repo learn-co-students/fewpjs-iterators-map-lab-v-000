b3e8b5506b0d414bf47fb2abebd4228eb7be6f93
Mohammed Noori <kmanoorib@gmail.com>
2020-01-01 07:26:13 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+// function capitalizeFirstLetter(string) {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+// }
+
 const titleCased = () => {
-  return tutorials
+  // return tutorials
+  // word[0] === word[0].toUpperCase()
+  return tutorials.map(function (tutorial) {
+    // return tutorial = tutorial.charAt(0).toUpperCase() + tutorial.slice(1);
+    // test wants every word to be uppercase. .split can break the string into each word, and then .join back into one string
+    tutorial = tutorial.split(' ');
+    for (let i = 0; i < tutorial.length; i++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+    }
+    return tutorial.join(' ');
+  })
 }
