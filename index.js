7df6193c7355e573a1d4033c39de055416e77e88
github username <constance.hyman@mchsi.com>
2020-02-16 20:14:36 -0800

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => tutorial.split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' '));
+  }
