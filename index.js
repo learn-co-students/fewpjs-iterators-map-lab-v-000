7cee6f864a1059ccf9ba0f86640c5fcc029ee89f
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-02 11:16:25 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(
+    tutorial => tutorial.split(" ").map(capitalizeWord).join(" ")
+  )
+}
+
+function capitalizeWord(word) {
+  return `${word[0].toUpperCase()}${word.slice(1)}`;
 }
