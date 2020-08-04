b7403b3eeb2ef85f015e5433209a3baaa4b3bc3d
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-08-04 12:06:56 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  let Tutorials = []
+  tutorials.map(function(tutorial) {
+    tutorial = tutorial.split(' ');
+    for (let i = 0; i < tutorial.length; i++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+    };
+    Tutorials.push(tutorial.join(' '))
+  });
+  return Tutorials
 }
+  
