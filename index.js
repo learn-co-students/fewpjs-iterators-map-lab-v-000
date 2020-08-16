c44915918c42b15e36ba0f86a491f24b39c163ee
Liz P <parisielizabeth@gmail.com>
2020-08-16 05:45:00 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+/* const titleCased = () => {
+//  return tutorials
+} 
+*/
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {tutorial = tutorial.split(' ');
+  
+  for (let i = 0; i < tutorial.length; i++){
+    tutorial[i] = `${tutorial[i].charAt(0).toUpperCase()}${tutorial[i].slice(1)}`;
+    }
+    return tutorial.join(' ');
+  });
+};
