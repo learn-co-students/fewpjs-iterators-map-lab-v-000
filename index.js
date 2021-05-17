ad30d5f338977c666b3eabd4282fd157f6cc65b0
capricious-slingshot <ephemere@protonmail.com>
2021-05-16 17:57:39 -0700

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map( tutorial => {
+    return tutorial.split(' ').map( word =>{
+      return word.substring(0,1).toUpperCase() + word.substring(1)
+    }).join(' ')
+  })
 }
